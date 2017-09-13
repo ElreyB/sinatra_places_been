4c91efd36aacf35325ced8099b3696d8b710645c
Elrey Belmonti and LDMacKrell <ldmackrell@>
2017-09-13 09:30:10 -0700

    add functionality to save places to an array.

--- lib/places_been.rb
+++ lib/places_been.rb
@@ -1,6 +1,15 @@
 class PlacesBeen
+  @@places = []
   attr_accessor(:name)
   def initialize(name)
     @name = name
+  end
+
+  def self.all
+    @@places
+  end
+
+  def save
+    @@places.push(self)
   end
 end
--- spec/places_been_spec.rb
+++ spec/places_been_spec.rb
@@ -2,10 +2,25 @@ require 'rspec'
 require 'places_been'
 
 describe(PlacesBeen) do
+  let(:place1){PlacesBeen.new("Japan")}
   describe('#name') do
     it 'will have a readable name'do
-    place1 = PlacesBeen.new("Japan")
       expect(place1.name).to(eq("Japan"))
     end
   end
+
+  describe('.places') do
+    it 'will have a readable places list' do
+      expect(PlacesBeen.all).to(be_a(Array))
+    end
+  end
+
+  describe('#save') do
+    it 'will save a place to the list of places' do
+      place1.save
+      expect(PlacesBeen.all).to(eq([place1]))
+    end
+
+  end
+
 end
